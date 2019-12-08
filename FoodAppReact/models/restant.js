d984dc3fe22f7a46de18290d280fba2e830e55c3
baertie <arno.baert@student.howest.be>
2019-12-08 17:15:56 +0100

    Models maken van de Restant en Categorie

---
+++ FoodAppReact/models/restant.js
@@ -0,0 +1,25 @@
+class Restant {
+  constructor(
+    id,
+    categoryIds,
+    date,
+    title,
+    imageUrl,
+    isGlutenFree,
+    isVegan,
+    isVegetarian,
+    isLactoseFree
+  ) {
+    this.id = id;
+    this.categoryIds = categoryIds;
+    this.date = date;
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.isGlutenFree = isGlutenFree;
+    this.isVegan = isVegan;
+    this.isVegetarian = isVegetarian;
+    this.isLactoseFree = isLactoseFree;
+  }
+}
+
+export default Restant;
